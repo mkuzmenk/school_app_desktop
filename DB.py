3a4ab99e587cdb55fbc3c78e0201848ba82dfeb1
SunJets <artemrabinin450@gmail.com>
2025-01-23 14:10:03 +0200

    Added User_Roles and Users tables.

--- DB.py
+++ DB.py
@@ -77,37 +77,73 @@ class Time_Table(Base):
     Time_Table_ID = Column(mysql.INTEGER(11), primary_key=True)
     Time_Table_User_REF = Column(mysql.INTEGER(11))
     Time_Table_GroupName = Column(mysql.VARCHAR(255))
-    Time_Table_Cabinet = Column(mysql.SMALLINT(6))
-    Time_Table_Start_Time = Column(mysql.DATETIME)
-    Time_Table_End_Time = Column(mysql.DATETIME)
+    Home_Work_TimeTable_REF = Column(mysql.INTEGER(11))
+    Home_Work_Desc = Column(mysql.VARCHAR(255))
+    Home_Work_Deadline = Column(mysql.DATE)
 
-    def __init__(self, id, user_ref, group_name, cabinet, start_time, end_time):
-        self.Time_Table_ID = id
-        self.Time_Table_User_REF = user_ref
-        self.Time_Table_GroupName = group_name
-        self.Time_Table_Cabinet = cabinet
-        self.Time_Table_Start_Time = start_time
-        self.Time_Table_End_Time = end_time
+    def __init__(self, id, name, user_ref, group_ref, timetable_ref, desc, deadline):
+        self.Home_Work_ID = id
+        self.Home_Work_Name = name
+        self.Home_Work_User_REF = user_ref
+        self.Home_Work_Group_REF = group_ref
+        self.Home_Work_TimeTable_REF = timetable_ref
+        self.Home_Work_Desc = desc
+        self.Home_Work_Deadline = deadline
 
 
     def __str__(self):
-        return f'{self.Time_Table_ID} - {self.Time_Table_GroupName}'
+        return f'{self.Home_Work_ID} - {self.Home_Work_Name}'
 
 
-class User_Group(Base):
-    __tablename__ = 'User_Group'
+class User_Roles(Base):
+    __tablename__ = 'User_Roles'
 
-    ID = Column(mysql.INTEGER(11), primary_key=True)
-    User_REF = Column(mysql.INTEGER(11))
-    Group_Name = Column(mysql.VARCHAR(255))
+    User_Role_ID = Column(mysql.SMALLINT(6), primary_key=True)
+    User_Role_Name = Column(mysql.VARCHAR(255), primary_key=True)
 
-    def __init__(self, id, user_ref, group_name):
-        self.ID = id
-        self.User_REF = user_ref
-        self.Group_Name = group_name
+    def __init__(self, id, name):
+        self.User_Role_ID = id
+        self.User_Role_Name = name
 
     def __str__(self):
-        return f'{self.ID} - {self.Group_Name}'
+        return f'{self.User_Role_ID} - {self.User_Role_Name}'
 
 
+class Users(Base):
+    __tablename__ = 'Users'
 
+    User_ID = Column(mysql.INTEGER(11), primary_key=True)
+    User_Login = Column(mysql.INTEGER(11))
+    User_Password = Column(mysql.VARCHAR(24))
+    User_First_Name = Column(mysql.VARCHAR(255))
+    User_Last_Name = Column(mysql.VARCHAR(255))
+    User_Surname = Column(mysql.VARCHAR(255))
+    User_Phone = Column(mysql.VARCHAR(255))
+    User_EMail = Column(mysql.VARCHAR(255))
+    User_Sex = Column(mysql.CHAR(1))
+    User_Birthday = Column(mysql.DATE)
+    User_Role = Column(mysql.SMALLINT(6))
+    User_Taxnumber = Column(mysql.INTEGER(11))
+    User_Desc = Column(mysql.VARCHAR(255))
+    User_Creation_Date = Column(mysql.DATETIME)
+    User_DLC = Column(mysql.DATETIME)
+
+    def __init__(self, id, login, password, first_name, last_name, surname, phone, email, sex, birthday, role, taxnumber, desc, creation_date, dlc):
+        self.User_ID = id
+        self.User_Login = login
+        self.User_Password = password
+        self.User_First_Name = first_name
+        self.User_Last_Name = last_name
+        self.User_Surname = surname
+        self.User_Phone = phone
+        self.User_EMail = email
+        self.User_Sex = sex
+        self.User_Birthday = birthday
+        self.User_Role = role
+        self.User_Taxnumber = taxnumber
+        self.User_Desc = desc
+        self.User_Creation_Date = creation_date
+        self.User_DLC = dlc
+
+    def __str__(self):
+        return f'{self.User_ID} - {self.User_First_Name} - {self.User_Last_Name} - {self.User_Login} - {self.User_Role}'
