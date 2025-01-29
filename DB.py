36737418994215be05695a19466aed92bc3ce7d3
mkuzmenk <mkuzmenko50@gmail.com>
2025-01-29 15:54:47 +0200

    finished models, have to add relationships now

--- DB.py
+++ DB.py
@@ -38,8 +38,8 @@ class AuthPermission(Base):
 
     id = Column(mysql.INTEGER, primary_key=True, autoincrement=True)
     name = Column(mysql.VARCHAR(255), nullable=False)
-    content_type_id = Column(mysql.INTEGER, nullable=False)
-    codename = Column(mysql.VARCHAR(100), nullable=False)
+    content_type_id = Column(mysql.INTEGER, nullable=False, unique=True)
+    codename = Column(mysql.VARCHAR(100), nullable=False, unique=True)
 
     auth_permissions_group = relationship('AuthGroupPermissions', back_populates='auth_permission')
 
@@ -71,8 +71,8 @@ class AuthGroupPermissions(Base):
     __tablename__ = 'auth_group_permissions'
 
     id = Column(mysql.BIGINT, primary_key=True, autoincrement=True)
-    group_id = Column(mysql.INTEGER, ForeignKey('auth_group.id'), nullable=False)
-    permission_id = Column(mysql.INTEGER, ForeignKey('auth_permission.id'), nullable=False)
+    group_id = Column(mysql.INTEGER, ForeignKey('auth_group.id'), nullable=False, unique=True)
+    permission_id = Column(mysql.INTEGER, ForeignKey('auth_permission.id'), nullable=False, unique=True)
 
     auth_group = relationship('AuthGroup', back_populates='auth_group_permissions')
     auth_permission = relationship('AuthPermission', back_populates='auth_permissions_group')
@@ -116,11 +116,12 @@ class Groups(Base):
 
     group_id = Column(mysql.INTEGER, primary_key=True, autoincrement=True)
     group_name = Column(mysql.VARCHAR(255), nullable=False)
-    group_teacher_id_id = Column(mysql.INTEGER, nullable=True)
+    group_teacher_id_id = Column(mysql.INTEGER, ForeignKey('users.user_id'), nullable=True)
 
-    def __init__(self, group_id, group_name):
+    def __init__(self, group_id, group_name, group_teacher_id_id):
         self.group_id = group_id
         self.group_name = group_name
+        self.group_teacher_id_id = group_teacher_id_id
 
     def __str__(self):
         return f'{self.group_id} - {self.group_name}'
@@ -139,26 +140,61 @@ class Homeworks(Base):
     home_work_timetable_ref = Column(mysql.INTEGER, nullable=True)
     home_work_user_ref_id = Column(mysql.INTEGER, nullable=True)
 
-    def __init__(self, homework_id, discipline_id, homework_text):
-        self.homework_id = homework_id
-        self.discipline_id = discipline_id
-        self.homework_text = homework_text
+    def __init__(self, home_work_id, home_work_name, home_work_topic, home_work_description, home_work_deadline,
+                 home_work_created_at, home_work_group_id_ref, home_work_timetable_ref, home_work_user_ref_id):
+
+        self.home_work_id = home_work_id
+        self.home_work_name = home_work_name
+        self.home_work_topic = home_work_topic
+        self.home_work_description = home_work_description
+        self.home_work_deadline = home_work_deadline
+        self.home_work_created_at = home_work_created_at
+        self.home_work_group_id_ref = home_work_group_id_ref
+        self.home_work_timetable_ref = home_work_timetable_ref
+        self.home_work_user_ref_id = home_work_user_ref_id
 
     def __str__(self):
-        return f'{self.homework_id} - Discipline: {self.discipline_id}'
+        return f'{self.home_work_id} - Group id ref: {self.home_work_group_id_ref}'
+
 
 class MarkTypes(Base):
     __tablename__ = 'mark_types'
 
     mark_type_id = Column(mysql.INTEGER, primary_key=True, autoincrement=True)
-    mark_name = Column(mysql.VARCHAR(255), nullable=False)
+    mark_type_name = Column(mysql.VARCHAR(255), nullable=False)
 
-    def __init__(self, mark_type_id, mark_name):
+    def __init__(self, mark_type_id, mark_type_name):
         self.mark_type_id = mark_type_id
-        self.mark_name = mark_name
+        self.mark_type_name = mark_type_name
 
     def __str__(self):
-        return f'{self.mark_type_id} - {self.mark_name}'
+        return f'{self.mark_type_id} - {self.mark_type_name}'
+
+
+class Marks(Base):
+    mark_id = Column(mysql.INTEGER, autoincrement=True, primary_key=True)
+    mark_value = Column(mysql.INTEGER, nullable=False)
+    mark_created_at = Column(mysql.DATE, nullable=False)
+    homework_id_ref = Column(mysql.INTEGER, ForeignKey('homeworks.home_work_id'), nullable=False)
+    mark_discipline_type_ref = Column(mysql.INTEGER, ForeignKey('disciplines.discipline_id'), nullable=False)
+    mark_student_id = Column(mysql.INTEGER, nullable=False)
+    mark_teacher_id = Column(mysql.INTEGER, nullable=False)
+    mark_type = Column(mysql.INTEGER, nullable=False)
+
+    def __init__(self, mark_id, mark_value, mark_created_at, homework_id_ref, mark_discipline_type_ref, mark_student_id,
+                 mark_teacher_id, mark_type):
+        self.mark_id = mark_id
+        self.mark_value = mark_value
+        self.mark_created_at = mark_created_at
+        self.homework_id_ref =homework_id_ref
+        self.mark_discipline_type_ref = mark_discipline_type_ref
+        self.mark_student_id = mark_student_id
+        self.mark_teacher_id = mark_teacher_id
+        self.mark_type = mark_type
+
+
+    def __str__(self):
+        return f'{self.mark_id} - value {self.mark_value} - student {self.mark_student_id}'
 
 
 class Users(Base):
@@ -278,38 +314,38 @@ Base.metadata.create_all(bind=engine)
 
 
 # function for test
-def get_all_roles():
-    conn = Session()
-
-    roles = conn.query(User_Roles).all()
-    for role in roles:
-        print(role)
-
-    conn.close()
-
-
-def check_relationship_roles():
-    conn = Session()
-
-    user_roles = conn.query(User_Roles).all()
-    for role1 in user_roles:
-        print(role1.User_Role_Name)
-
-        if role1.all_users:
-            for user in role1.all_users:
-                print(user.User_Login, user.User_First_Name)
-        else:
-            print('no users here')
-
-        print()
-
-    conn.close()
-
-
-def test():
-    get_all_roles()
-    print('fk check')
-    check_relationship_roles()
+# def get_all_roles():
+#     conn = Session()
+#
+#     roles = conn.query(User_Roles).all()
+#     for role in roles:
+#         print(role)
+#
+#     conn.close()
+#
+#
+# def check_relationship_roles():
+#     conn = Session()
+#
+#     user_roles = conn.query(User_Roles).all()
+#     for role1 in user_roles:
+#         print(role1.User_Role_Name)
+#
+#         if role1.all_users:
+#             for user in role1.all_users:
+#                 print(user.User_Login, user.User_First_Name)
+#         else:
+#             print('no users here')
+#
+#         print()
+#
+#     conn.close()
 
 
-test()
+# def test():
+#     get_all_roles()
+#     print('fk check')
+#     check_relationship_roles()
+#
+#
+# test()
