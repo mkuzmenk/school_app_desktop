80559ca5e7a847736c980e2a370bede591c05327
mkuzmenk <mkuzmenko50@gmail.com>
2025-01-31 21:56:17 +0200

    have done raw MVC pattern

---
+++ Controller.py
@@ -0,0 +1,6 @@
+
+
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from window import Window
+from Model import Model
+from Controller import Controller
 
-Window().run()
+if __name__ == '__main__':
+    model = Model()
+    view = Window()
+    controller = Controller(model, view)
+
+    # also need to add controller into view! view.controller = controller
+
+    view.run()
