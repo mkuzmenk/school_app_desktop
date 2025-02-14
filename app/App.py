6f962379f0ba83e8ff90ede27247080b1bbf8d5c
SunJets <artemrabinin450@gmail.com>
2025-02-14 09:44:24 +0200

    Removed commentary in App

--- app/App.py
+++ app/App.py
@@ -1,14 +1,11 @@
 from controller.Controller import Controller
 from model.Model import Model
 from view.authorization_window.AuthorizationWindow import AuthorizationWindow
-from view.main_windows.teacher.TeacherMode import TeacherMode
 
 
 class App:
     def __init__(self):
-        # self.view = AuthorizationWindow()
-
-        self.view = TeacherMode(4)
+        self.view = AuthorizationWindow()
 
         self.model = Model()
 
